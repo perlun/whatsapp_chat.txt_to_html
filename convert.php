e10ce3df261e115299ac93116b363705164b689f
Per Lundberg <perlun@gmail.com>
2021-06-26 11:53:45 +0300

    Removed dead/unused code

--- convert.php
+++ convert.php
@@ -149,45 +149,9 @@ if (isset($_FILES['whatsapp'])) {
             $me = "";
         }
 
-        function SearchInFile($searchfor)
-        {
-            $searchfor = 'name';
-
-            // the following line prevents the browser from parsing this as HTML.
-            header('Content-Type: text/plain');
-
-            // get the file contents, assuming the file to be readable (and exist)
-            $contents = file_get_contents($file);
-
-            // escape special characters in the query
-            $pattern = preg_quote($searchfor, '/');
-
-            // finalise the regular expression, matching the whole line
-            $pattern = "/^.*$pattern.*\$/m";
-
-            // search, and store all matching occurences in $matches
-            if (preg_match_all($pattern, $contents, $matches)) {
-                echo "Found matches:\n";
-                echo implode("\n", $matches[0]);
-            } else {
-                echo "No matches found";
-            }
-        }
-
-        function FindCode($pattern)
-        {
-            return false;
-        }
-
         $handle = fopen($file, "r");
         if ($handle) {
             while (($line = fgets($handle)) !== false) {
-                // process the line read.
-                //echo("<div class=\"all\">".$line."</div>");
-                //for ($x = 0; $x < strlen($line); $x++) {
-                //    echo(ord($line[$x]).".");
-                //}
-
                 $filename = "";
                 $location = "";
                 if (strpos($line, "<attached: ") > 0) {
